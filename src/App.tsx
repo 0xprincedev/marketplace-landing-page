a6d97172936cbba549600ef039a130d840299d2d
0xprincedev <enthusiast0807@gmail.com>
2023-02-24 01:31:01 -0500

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { ToastContainer } from 'react-toastify'
 import { createClient, configureChains, mainnet, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-// import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-// import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-// import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import 'react-toastify/dist/ReactToastify.css'
 
 import Home from 'pages/Home'
@@ -19,28 +16,7 @@ const App = () => {
 	// Set up client
 	const client = createClient({
 		autoConnect: true,
-		connectors: [
-			new MetaMaskConnector({ chains }),
-			// new CoinbaseWalletConnector({
-			// 	chains,
-			// 	options: {
-			// 		appName: 'wagmi',
-			// 	},
-			// }),
-			// new WalletConnectConnector({
-			// 	chains,
-			// 	options: {
-			// 		qrcode: true,
-			// 	},
-			// }),
-			// new InjectedConnector({
-			// 	chains,
-			// 	options: {
-			// 		name: 'Injected',
-			// 		shimDisconnect: true,
-			// 	},
-			// }),
-		],
+		connectors: [new MetaMaskConnector({ chains })],
 		provider,
 		webSocketProvider,
 	})
